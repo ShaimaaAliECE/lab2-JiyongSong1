7af0bee0cc46f9143922d32c9ccb06cf6b1d2457
jsong386 <jsong386@uwo.ca>
2021-11-11 19:25:08 -0500

    Changes made to Connect4 app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,149 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component{
+  state = {
+    rows: 6,
+    columns: 7,
+    moves: [],
+    currentPlayer: 'red',
+  };
+
+  //reset function
+  reset = () => {
+    this.setState({moves: [], winner: null });
+  }
+
+  //check for win
+  checkWin = (x, y, player) => {
+    let xScore = 1;
+    let yScore = 1;
+
+    //right horizontal win
+    for (let column = x + 1; column < x + 4; column++){
+      const check = this.getPiece (column, y);
+      if (check && check.player === player){
+        xScore++;
+      }
+      else {
+        break;
+      }
+    }
+    //left horizontal win
+    for (let column = x - 1; column > x - 4; column--){
+      const check = this.getPiece (column, y);
+      if (check && check.player === player){
+        xScore++;
+      }
+      else {
+        break;
+      }
+    }
+    //win if there are 4 pieces in a row
+    if (xScore >= 4){
+      this.setState ({winner: player});
+      return true;
+    }
+
+    //upside vertical win
+    for (let row = y + 1; row < y + 4; row++){
+      const check = this.getPiece (x, row);
+      if (check && check.player === player){
+        yScore++;
+      }
+      else {
+        break;
+      }
+    }
+    //downward vertical win
+    for (let row = y - 1; row > y - 4; row--){
+      const check = this.getPiece (x, row);
+      if (check && check.player === player){
+        yScore++;
+      }
+      else {
+        break;
+      }
+    }
+    //win if there are 4 pieces in a row
+    if (yScore >= 4){
+      this.setState ({winner: player});
+      return true;
+    }
+  }
+
+  //function to get the piece
+  getPiece = (x, y) => {
+    const list = this.state.moves.filter ((item) => {
+      return (item.x === x && item.y === y);
+    });
+    return list[0];
+  }
+
+  //function to place the piece
+  makeMove = (x, y) => {
+    const { currentPlayer } = this.state;
+    //next player is yellow if current player is red, otherwise next player is red by default
+    const nextPlayer = currentPlayer === 'red' ? 'yellow' : 'red';
+    //must check for a win at each move
+    this.setState({moves: this.state.moves.concat({x, y, player: currentPlayer }), currentPlayer: nextPlayer}, () => this.checkWin(x, y, currentPlayer));
+  }
+
+  //the board
+  renderBoard(){
+    const { rows, columns, winner } = this.state;
+    const rowView = [];
+
+    //create the columns and rows
+    for (let row = 0; row < this.state.rows; row++){
+      const columnView = [];
+
+      for (let column = 0; column < this.state.columns; column++){
+        const piece = this.getPiece(column, row);
+
+        //displays the pieces when user clicks on them
+        columnView.push(
+          <div onClick={() => {this.makeMove(column,row)}} style={{width: '6vw', height: '6vw', backgroundColor: '#87ceeb', display: 'flex', padding: 3}}>
+            <div style={{borderRadius: '50%', backgroundColor: 'white', flex: 1, display: 'flex'}}>
+              {piece ? <div style = {{backgroundColor: piece.player, flex: 1, borderRadius: '50%'}}/> : undefined}
+            </div>
+          </div>
+        );
+      }
+      rowView.push(
+        <div style = {{display: 'flex', flexDirection: 'row' }}>{columnView}</div>
+      );
+    }
+
+    return(
+      //displays the winner
+      <div style={{backgroundColor: 'red', display: 'flex', flexDirection: 'column' }}>        
+        {winner && <div onClick = {this.reset} style={{position: 'absolute', left: 0, right: 0, bottom: 0, top: 0, zIndex: 3, backgroundColor: 'rgba(0, 150, 0, .3)', display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'black', fontSize: '5vw'}}>{winner} WINS</div>}
+        {rowView}
+      </div>
+    );
+  }
+
+  props: { style: any; };
+  render() {
+    const { style } = this.props;
+
+    return (
+      <div style={style ? Object.assign({}, styles.container, style) : styles.container}>
+        <div>
+          {this.renderBoard()}
+        </div>        
+      </div>
+    );
+  }
 }
 
-export default App;
+const styles = {
+  container: {
+    height: '100%',
+    padding: 5,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+};
